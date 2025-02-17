92e06870beacfc9ce1180bbe45d07270f09fac3c
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-17 21:51:01 +0200

    modified error and exit messages, moved some code outside the try block

--- main.py
+++ main.py
@@ -2,41 +2,48 @@ from sys import exit
 
 
 def main() -> None:
+    exit_message: str = 'Exiting program...'
     while True:
         try:
             user_input: str = input('Enter a positive integer: ')
+
             if user_input == 'exit':
-                print('Thanks for trying my program!')
+                print(exit_message)
                 exit()
+
             start: int = int(user_input)
-            if start <= 0:
-                print('Integer must be positive...')
-                continue
-            
-            string: str = user_input
-            sum: int = 0
-            flag: bool = True
-            lst: list[int] = []
-            while sum != 1:
-                sum = 0
-                for char in string:
-                    sum += int(char) ** 2
-                lst.append(sum)
-                string = str(sum)
-                print(sum)
-                if len(set(lst)) != len(lst):
-                    flag = False
-                    break
-            if flag:
-                print(f'{start} is a happy number!')
-            else:
-                print(f'{start} is not a happy number!')
+
         except ValueError:
-            print('Invalid input...')
+            print('Please enter valid input...')
             continue
+
         except KeyboardInterrupt:
-            print('Exiting...')
+            print(exit_message)
             exit()
+
+        if start <= 0:
+            print('Please enter a positive integer...')
+            continue
+
+        string: str = user_input
+        sum: int = 0
+        flag: bool = True
+        lst: list[int] = []
+        while sum != 1:
+            sum = 0
+            for char in string:
+                sum += int(char) ** 2
+            lst.append(sum)
+            string = str(sum)
+            print(sum)
+            if len(set(lst)) != len(lst):
+                flag = False
+                break
+
+        if flag:
+            print(f'{start} is a happy number!')
+        else:
+            print(f'{start} is not a happy number!')
 
 
 if __name__ == '__main__':
